805cbf10c3429e6f96f085fa1b8ed9cb65ae52ed
victomina <victomina@protonmail.com>
2026-03-15 01:29:35 -0300

    Typescript -  Adicionando dependencias de instalação do typescript

--- Typescript/index.js
+++ Typescript/index.js
@@ -1,6 +1,23 @@
 const guitar = {
     title: 'Electric Guitar Marshall DSL40CR',
+    price: 799.03,
+    description: 'The Marshall DSL40CR is a versatile and powerful electric guitar amplifier that delivers classic Marshall tone with modern features. It features a 40-watt output, two channels (Classic Gain and Ultra Gain), and a built-in reverb effect. The amplifier also includes a footswitch for easy channel switching and a headphone output for silent practice. With its iconic design and exceptional sound quality, the Marshall DSL40CR is perfect for both beginners and experienced guitarists.',
     brand: 'Fender',
     model: 'Stratocaster',
     year: 2020
 };
+
+const guitar2 = {
+    title: 'Electric Guitar Marshall DSL40CR',
+    price: 799.03,
+    description: 'The Marshall DSL40CR is a versatile and powerful electric guitar amplifier that delivers classic Marshall tone with modern features. It features a 40-watt output, two channels (Classic Gain and Ultra Gain), and a built-in reverb effect. The amplifier also includes a footswitch for easy channel switching and a headphone output for silent practice. With its iconic design and exceptional sound quality, the Marshall DSL40CR is perfect for both beginners and experienced guitarists.',
+    brand: 'Fender',
+    model: 'Stratocaster',
+    year: 2020
+};
+
+guitar.price = 'R$ 876,09'
+
+const cartPrice = guitar.price + guitar2.price
+
+console.log(cartPrice, "cartPrice")
