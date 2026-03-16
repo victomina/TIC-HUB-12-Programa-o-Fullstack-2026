d2aa0fa33a8d16d0d352cb96386ecc42a5bdc60b
victomina <victomina@protonmail.com>
2026-03-16 02:27:07 -0300

    Adicionando mudança para verificação posterior, pois dava erro no objeto categoria retornando da classe product

--- Typescript/src/model/product.model.ts
+++ Typescript/src/model/product.model.ts
@@ -1,8 +1,9 @@
-import type { Category } from "./category.model";
-
+import { Category } from "./category.model";
 export class Product {
 
-    constructor(public title: string, public price: number, public imagemeUrl?: string, public category: Category, public discount: number ){ }
+    constructor(public title: string, public price: number, public imagemeUrl?: string, public category?: Category, public discount?: number ){
+
+    }
     
     priceWithDiscountApplied(discount: number): number {
         return this.price - (this.price * discount);
