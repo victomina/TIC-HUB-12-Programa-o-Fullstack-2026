83323a64ee0bab217ccfcd8169ffac55c74ea373
victomina <victomina@protonmail.com>
2026-04-05 04:10:54 -0300

    Adicionando botõesde configurações

--- ECommerce/src/App.vue
+++ ECommerce/src/App.vue
@@ -4,24 +4,43 @@ including the header, main content area, and footer. *Essa é uma bibliotexca de
 projeto ECommerce. * Ela serve como ponto de entrada para a aplicação Vue.js e conterá * a estrutura
 geral do aplicativo, incluindo o cabeçalho, área de conteúdo principal e rod */
 <script lang="ts">
+import { Product } from './model/product.model'
+import ProductCard from './components/ProductCard.vue'
+import { Cart } from './model/cart.model'
+
 export default {
+  components: {
+    ProductCard,
+  },
   data() {
     return {
-      cart: {
-        items: [],
-        total: 0,
-      },
-      product: {
-        name: 'Guitarra Encantada',
-        price: 100,
-        description: 'Product Description',
-        discount: 0,
-      },
+      cart: new Cart(),
+      products: [
+        new Product(
+          'Guitarra Encantada',
+          100,
+          'Uma guitarra mágica que toca sozinha e encanta a todos com suas melodias. Perfeita para músicos de todos os níveis.',
+          0,
+        ),
+        new Product(
+          'Bateria Mágica',
+          200,
+          'Uma bateria que toca sozinha e cria ritmos incríveis. Ideal para bateristas de todos os estilos.',
+          10,
+        ),
+      ],
     }
   },
   methods: {
-    addItem() {
-      this.cart.total += 1
+    addItem(product: Product) {
+      const existItem = this.cart.list.some((item) => item.product.name === product.name)
+      if (existItem) {
+        const item = this.cart.list.find((item) => item.product.name === product.name)
+        return item ? (item.quantity += 1) : null
+      } else {
+        this.cart.list.push({ product, quantity: 1 })
+        this.cart.total += 1
+      }
     },
     decItem() {
       this.cart.total -= 1
@@ -32,22 +51,17 @@ export default {
 
 <template>
   <main>
-    <article>
-      <h1>{{ product.name }}</h1>
-      <p>{{ product.description }}</p>
-      <p>Price: ${{ product.price }}</p>
-      <p>Discount: {{ product.discount }}%</p>
-    </article>
-    <button @click="addItem()">Adicionar ao Carrinho</button>
-    <button @click="decItem()">Remover do Carrinho</button>
-
-    <article>
-      <h1>Welcome to the ECommerce App</h1>
-      <p>total: {{ cart.total + 1 }}</p>
-    </article>
-    <button @click="addItem()">Adicionar ao Carrinho</button>
-    <button @click="decItem()">Remover do Carrinho</button>
+    <section class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+      <!-- //cards que mostrm em uma coluna   -->
+      <div v-for="product in products" :key="product.name">
+        <ProductCard :product="product" @on-click="addItem" />
+      </div>
+    </section>
   </main>
+  <h1>Carrinho</h1>
+  <div v-for="item in cart.list" :key="item.product.name">
+    <h3>{{ item.product.name }}</h3>
+  </div>
+  <p>Total: {{ cart.total }}</p>
+  <Button :label="'Primevue'"></Button>
 </template>
-
-<style></style>
