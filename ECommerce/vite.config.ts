ab07b87b2b7edd5955f7a09a491275cd3953540a
victomina <victomina@protonmail.com>
2026-04-10 03:50:15 -0300

    configuração do Vite para acessar a API

--- ECommerce/vite.config.ts
+++ ECommerce/vite.config.ts
@@ -13,4 +13,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  server:{
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:3000/api',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
+  }
 })
